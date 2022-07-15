c7cb677720d9bd148bc476f01e1cfdec9353aba2
maxdevaine <maxdevaine@volny.cz>
2022-07-15 15:11:10 +0200

    Create exchange-delete-old-emails.ps1

---
+++ powershell/exchange-delete-old-emails.ps1
@@ -0,0 +1,18 @@
+#!/bin/bash
+# author: Max Devaine <maxdevaine@gmail.com>
+# created: 2022/07
+# license: GNU GPLv3
+# description
+# Delete all emails in selected date range
+# search-mailbox is limited to 10000 results, so this is why cycle with counter ("-le 2" = up to 20000 items)
+
+$var = 1
+$mailbox = user1
+
+while ($var -le 2) {
+   search-mailbox -identity $mailbox -searchquery {received:01/04/2015..05/10/2021} -deletecontent -force
+   Write-Host The value of Var is: $var
+   $var++
+}
+
+Write-Host End of While loop.
