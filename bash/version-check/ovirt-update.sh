38083ff2a9d5de592bc483a660a233ab66c20c4c
maxdevaine <maxdevaine@volny.cz>
2022-11-01 09:35:08 +0100

    Update ovirt-update.sh

--- bash/version-check/ovirt-update.sh
+++ bash/version-check/ovirt-update.sh
@@ -2,7 +2,7 @@
 # author: Max Devaine <maxdevaine@gmail.com>
 # created: 2019/06
 # license: GNU GPLv3
-# description: parse html web page to check actual version of the oVirt, notify via email in case of new version
+# description: parse html web page to check current version of the oVirt, notify via email in case of new version
 
 STATFILE=/home/version-check/scripts/tmp/ovirt-curver
 CURVER=`cat ${STATFILE}`
