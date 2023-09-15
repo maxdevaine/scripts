ac260880b44f98bfe1a5f82bd2cee7a166d6507c
maxdevaine <maxdevaine@volny.cz>
2023-09-15 14:23:46 +0200

    Create veeam-update.sh

---
+++ bash/version-check/veeam-update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# author: Max Devaine <maxdevaine@gmail.com>
+# created: 2023/08
+# license: GNU GPLv3
+# description: parse html web page to check current version of Helios CLA IQ product, notify via email in case of new version
+
+CONTACT=`head -1 /home/version-check/scripts/_contact`
+STATFILE=/home/version-check/scripts/tmp/veeam-curver
+CURVER=`cat ${STATFILE}`
+ROK=$(date '+%Y')
+URLVERS="https://www.veeam.com/kb2680"
+NEWVER=`wget -qO- ${URLVERS} |grep "<tr><td>Veeam Backup" |tr -s "<\tr>" "\n" | grep "Veeam Backup" | head -1`
+
+
+if [ "$CURVER" != "$NEWVER" ]; then
+    echo "Je nová verze Veeam: ""$NEWVER"" !!!"
+    echo -e "New version of Veeam:\n ""$NEWVER""\n""See KB:""\n""$URLVERS"" " | mail -s "${NEWVER}" ${CONTACT}
+    echo "$NEWVER" > ${STATFILE}
+  else
+    echo "Není nová verze, stále je aktuální ${CURVER}"
+fi
