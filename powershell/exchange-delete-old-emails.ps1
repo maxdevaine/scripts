efd733aa09f689fd9cf950c3b9817615180d1240
Max Devaine <maxdevaine@volny.cz>
2024-07-09 16:24:38 +0200

    Update exchange-delete-old-emails.ps1
    Modify script to use parameters

--- powershell/exchange-delete-old-emails.ps1
+++ powershell/exchange-delete-old-emails.ps1
@@ -3,16 +3,59 @@
 # license: GNU GPLv3
 # description
 # Delete all emails in selected date range
-# search-mailbox is limited to 10000 results, so this is why cycle with counter ("-le 2" = up to 20000 items)
+# search-mailbox is limited to 10000 results, so this is why cycle with counter (for example "-le 2" = up to 20000 items)
 
-$var = 1
-$mailbox = user1
+<#
+.SYNOPSIS
+    .
+.DESCRIPTION
+    Script delete all emails older than defined days.
+.PARAMETER MailboxName
+    Name of mailbox
+.PARAMETER Days
+    Specify count of days (older than x days)
+.EXAMPLE
+    C:\PS> 
+     -MailboxName User -Days 365 -LoopCountTotal 10
+.NOTES
+    Author: Max Devaine
+    Date:   June 9, 2024
+#>
 
-while ($var -le 2) {
-   search-mailbox -identity $mailbox -searchquery {received:01/04/2015..05/10/2021} -deletecontent -force
-   #Search-Mailbox -Identity $mailbox -SearchQuery 'Received<="2022-08-31" AND kind:email' -DeleteContent -force
-   Write-Host The value of Var is: $var
-   $var++
+
+Param($MailboxName,$Days,$LoopCountTotal)
+
+$example = "Example: $PSCommandPath -MailboxName User -Days 365 -LoopCountTotal 10"
+
+if ($MailboxName -eq $null) {
+  Write-Host Please specify the MailboxName
+  Write-Host $example
+  Exit
 }
 
-Write-Host End of While loop.
+if ($Days -eq $null) {
+  Write-Host Please specify the Days value "(older than x days)"
+  Write-Host $example
+  Exit
+}
+
+if ($LoopCountTotal -eq $null) {
+  Write-Host Please specify the LoopCountTotal value "(how many times run loop)"
+  Write-Host $example
+  Exit
+}
+
+
+$LoopCount = 1
+$DaysAgoCount = (Get-Date).AddDays(-$Days)
+$DaysAgo = Get-Date -Date $DaysAgoCount -Format "MM/dd/yyyy"
+
+while ($LoopCount  -le $LoopCountTotal) {
+   Write-Host The loop value is: $LoopCount/$LoopCountTotal
+   Write-Host Remove all emails older than: $DaysAgo "(MM/dd/yyyy)"
+   #search-mailbox -identity $MailboxName -searchquery {received:01/04/2015..07/07/2023} -deletecontent -force
+   Search-Mailbox -Identity $MailboxName -SearchQuery "Received<=$DaysAgo AND kind:email" -DeleteContent -force
+   $LoopCount++
+}
+
+Write-Host End of loop.
