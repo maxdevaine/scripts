878dbdb1f07b2e1a5b2cd1cd81653a843dada25a
Max Devaine <maxdevaine@volny.cz>
2024-07-11 23:30:23 +0200

    Update exchange-delete-old-emails.ps1
    Add example of batch file for windows scheduler

--- powershell/exchange-delete-old-emails.ps1
+++ powershell/exchange-delete-old-emails.ps1
@@ -4,6 +4,8 @@
 # description
 # Delete all emails in selected date range
 # search-mailbox is limited to 10000 results, so this is why cycle with counter (for example "-le 2" = up to 20000 items)
+# you can create batch file for windows scheduler, for example:
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command ". 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'; Connect-ExchangeServer -auto; . '"C:\Exchange_Scripts\exchange-delete-old-emails.ps1"' -MailboxName mailbox -Days 365 -LoopCountTotal 5"
 
 <#
 .SYNOPSIS
